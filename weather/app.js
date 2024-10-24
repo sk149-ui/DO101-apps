e0c57ffdc2c92d6d1145c9ddc6d04cd8361266d6
sk149-ui <sk149@gwmail.gwu.edu>
2024-10-24 13:52:48 -0400

    Fix syntax error

--- weather/app.js
+++ weather/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-
+app.use('/', indexRouter);
 var app = express();
 
 // view engine setup
